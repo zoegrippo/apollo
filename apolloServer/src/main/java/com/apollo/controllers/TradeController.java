e358e9c3ed2aa1d79c89897a4788ce2b8df687df
Zoe Grippo <zoe.grippo@gmail.com>
2018-07-31 21:41:02 +0000

    added requestbody and pathname

--- apolloServer/src/main/java/com/apollo/controllers/TradeController.java
+++ apolloServer/src/main/java/com/apollo/controllers/TradeController.java
@@ -6,10 +6,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin // allows requests from all domains
@@ -29,7 +26,7 @@ public class TradeController {
 
     @ApiOperation(value = "createNewTrade", nickname = "createNewTrade")
     @RequestMapping(value = "/trade", method = RequestMethod.POST)
-    public ResponseEntity<?> createNewStrategy(Trade t) {
+    public ResponseEntity<?> createNewStrategy(@RequestBody Trade t) {
         try {
             return new ResponseEntity<>(service.createOrUpdate(t), HttpStatus.OK);
         } catch (Exception e) {
@@ -39,7 +36,7 @@ public class TradeController {
 
     @ApiOperation(value = "getByStrategyId", nickname = "getByStrategyId")
     @RequestMapping(value = "/trade/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> getByStrategyId(int id) {
+    public ResponseEntity<?> getByStrategyId(@PathVariable("id") int id) {
         try {
             return new ResponseEntity<>(service.getTradeByStrategy(id), HttpStatus.OK);
         } catch (Exception e) {
